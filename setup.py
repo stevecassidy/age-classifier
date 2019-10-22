f114c8d8ae291e957d91b696930b6f7041b84f1c
Steve Cassidy <steve@SCI-9025.local>
2019-10-22 13:14:28 +1100

    initial checkin

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Determine brain age from MEG data',
+    author='Steve Cassidy',
+    license='BSD-3',
+)
